6886d6f64b7be86b8280f560bfef13c7557f10f1
Victor Jefferies <victorjefferies@MacBook-Pro-9.local>
2018-10-01 16:24:53 +0100

    Using prototype

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -1,18 +1,25 @@
 describe("FizzBuzz", function() {
+
+  var fizzbuzz;
+
+  beforeEach(function() {
+    fizzbuzz = new FizzBuzz();
+  }); 
+
   it("should return 'fizz' when passed 3", function() {
-    expect(FizzBuzz(3)).toEqual("fizz");
+    expect(fizzbuzz.play(3)).toEqual("fizz");
   });
 
   it("should return 'buzz' when passed 5", function() {
-    expect(FizzBuzz(5)).toEqual('buzz');
+    expect(fizzbuzz.play(5)).toEqual('buzz');
   });
 
   it("should return 4 when passed 4", function() {
-    expect(FizzBuzz(4)).toEqual(4);
+    expect(fizzbuzz.play(4)).toEqual(4);
   });
 
   it("should return 'fizzbuzz' when passed 15", function() {
-    expect(FizzBuzz(15)).toEqual('fizzbuzz')
+    expect(fizzbuzz.play(15)).toEqual('fizzbuzz')
   });
 
 });
--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,14 +1,32 @@
-function FizzBuzz(num) {
-  if (num % 3 == 0 && num % 5 == 0) {
-    return 'fizzbuzz';
-  }
-  else if (num % 3 == 0) {
-    return 'fizz';
-  }
-  else if (num % 5 == 0) {
-     return 'buzz';  
-  }
-  else {
+// function FizzBuzz(num) {
+//   if (num % 3 == 0 && num % 5 == 0) {
+//     return 'fizzbuzz';
+//   }
+//   else if (num % 3 == 0) {
+//     return 'fizz';
+//   }
+//   else if (num % 5 == 0) {
+//      return 'buzz';  
+//   }
+//   else {
+//     return num;
+//   }
+// };
+
+function FizzBuzz() {}
+
+FizzBuzz.prototype.play = function(num) {
+  if (this._isDivisibleBy(15, num)) {
+    return "fizzbuzz";
+  } else if (this._isDivisibleBy(5, num)) {
+    return "buzz";
+  } else if (this._isDivisibleBy(3, num)) {
+    return "fizz";
+  } else {
     return num;
   }
+};
+
+FizzBuzz.prototype._isDivisibleBy = function(testNumber, num) {
+  return num % testNumber === 0;
 };
